7832e3e5f540e9adbb4947c7d87af540cf80125f
abner <abner1724@gmail.com>
2021-03-22 11:56:58 +0900

    asr and ver

---
+++ asr/run.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Abner Hernandez (abner@snu.ac.kr) Seoul National University
+
+. ./path.sh || exit 1
+. ./cmd.sh || exit 1
+nj=14
+
+thread_nj=1
+
+# Test-time language model order
+lm_order=2
+# Word position dependent phones?
+pos_dep_phones=true
+. utils/parse_options.sh || exit 1
+
+
+Leaves=800
+Gauss=9000
+
+
+home_dir=/home/abner/kaldi/egs/torgo/asr #adjust to your directory
+data_dir=$home_dir/data  
+feat_dir=$home_dir/mfcc
+exp_dir=$home_dir/exp
+lang=$data_dir/lang
+lang_test=$data_dir/lang_test
+
+
+stage=1
+stop_stage=1
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo
+    echo "===== PREPARING Language DATA ====="
+    echo
+    # Prepare ARPA LM and vocabulary using SRILM
+    local/torgo_prepare_lm.sh --order ${lm_order} || exit 1
+    
+    # Prepare the lexicon and various phone lists
+    # Pronunciations for OOV words are obtained using a pre-trained Sequitur model
+    local/torgo_prepare_dict.sh || exit 1
+    echo ""
+    echo "=== Preparing data/lang and data/local/lang directories ..."
+    echo ""
+    
+    utils/prepare_lang.sh --position-dependent-phones $pos_dep_phones \
+	    data/local/dict '!SIL' data/local/lang data/lang || exit 1
+    
+    # Prepare G.fst and data/{train,test} directories
+    local/torgo_prepare_grammar.sh "test" || exit 1
+
+fi    
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo
+    echo "===== FEATURES EXTRACTION ====="
+    echo
+    # Making feats.scp files
+    mfccdir=mfcc
+    utils/validate_data_dir.sh data/train 
+    utils/fix_data_dir.sh data/train       
+    utils/validate_data_dir.sh data/test    
+    utils/fix_data_dir.sh data/test     
+
+
+    steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train exp/make_mfcc/train $mfccdir
+    steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" data/test exp/make_mfcc/test $mfccdir
+
+    # Making cmvn.scp files
+    steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
+    steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
+fi    
+
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+	steps/train_mono.sh --nj $nj --cmd "$train_cmd" --cmvn-opts "$cmvn_opts" \
+		$data_dir/train $lang $exp_dir/mono
+        steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                $data_dir/train $lang $exp_dir/mono $exp_dir/mono_ali
+        steps/train_deltas.sh --cmd "$train_cmd" --cmvn-opts "$cmvn_opts" \
+                $Leaves $Gauss $data_dir/train $lang $exp_dir/mono_ali $exp_dir/tri1
+        steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                $data_dir/train $lang $exp_dir/tri1 $exp_dir/tri1_ali
+        steps/train_deltas.sh --cmd "$train_cmd" --cmvn-opts "$cmvn_opts" \
+                $Leaves $Gauss $data_dir/train $lang $exp_dir/tri1_ali $exp_dir/tri2
+        steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                $data_dir/train $lang $exp_dir/tri2 $exp_dir/tri2_ali
+        steps/train_lda_mllt.sh --cmd "$train_cmd" --cmvn-opts "$cmvn_opts" \
+                $Leaves $Gauss $data_dir/train $lang $exp_dir/tri2_ali $exp_dir/tri3
+        steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                $data_dir/train $lang $exp_dir/tri3 $exp_dir/tri3_ali
+        steps/train_sat.sh --cmd "$train_cmd" \
+                $Leaves $Gauss $data_dir/train $lang $exp_dir/tri3_ali $exp_dir/tri4
+        steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
+                $data_dir/train $lang $exp_dir/tri4 $exp_dir/tri4_ali	 
+fi
+
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then    
+    # decode
+    utils/mkgraph.sh $lang_test $exp_dir/tri4 $exp_dir/tri4/graph
+    steps/decode_fmllr.sh --config conf/decode.config --nj 1 --cmd "$decode_cmd"  --num-threads $thread_nj --scoring_opts "$scoring_opts" \
+            $exp_dir/tri4/graph $data_dir/test $exp_dir/tri4/decode
+    cat exp/tri4/decode/scoring_kaldi/best_wer
+fi 
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo ""
+    echo "=== Neural Network models ..."
+    echo "--- nnet: Deep Neural Network (dnn)"
+    local/nnet/run_dnn.sh --nj 14
+    echo "Iteration 1 word-error rate"
+    cat exp/tri4/graph exp/dnn4b_pretrain-dbn_dnn_smbr/decode_it1/scoring_kaldi/best_wer
+    echo "Iteration 3 word-error rate"
+    cat exp/tri4/graph exp/dnn4b_pretrain-dbn_dnn_smbr/decode_it3/scoring_kaldi/best_wer
+    echo "Iteration 5 word-error rate"
+    cat exp/tri4/graph exp/dnn4b_pretrain-dbn_dnn_smbr/decode_it5/scoring_kaldi/best_wer
+fi
+
---
+++ ver/run.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+. ./cmd.sh
+. ./path.sh
+
+
+set -e # exit on error
+
+data=data
+nj=7
+mfccdir=mfcc
+
+
+
+stage=4
+stop_stage=6
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+	echo "=== Extract MFCC & Compute VAD ==="
+	for x in train test; do
+		steps/make_mfcc.sh --cmd "$train_cmd" --nj $nj data/$x exp/make_mfcc/$x $mfccdir
+		sid/compute_vad_decision.sh --nj $nj --cmd "$train_cmd" data/$x exp/make_mfcc/$x $mfccdir
+  		utils/fix_data_dir.sh data/$x
+	done
+fi
+
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+	echo "== Train UBM =="
+	# train diag ubm
+	sid/train_diag_ubm.sh --nj $nj --cmd "$train_cmd" \
+		data/train 1024 exp/diag_ubm_1024
+
+	#train full ubm
+	sid/train_full_ubm.sh --nj $nj --cmd "$train_cmd" data/train \
+		exp/diag_ubm_1024 exp/full_ubm_1024
+fi
+
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+	echo "== Train and Extract ivectors =="
+	#train ivector
+	sid/train_ivector_extractor.sh --cmd "$train_cmd" --nj 1\
+		--num-iters 5 exp/full_ubm_1024/final.ubm data/train exp/extractor_1024
+
+	#extract ivector
+	sid/extract_ivectors.sh --cmd "$train_cmd" --nj $nj \
+		exp/extractor_1024 data/train exp/ivector_train_1024
+
+ 	echo " "
+	echo "Finished training and extracting ivectors.."	
+	echo "== Training PLDA =="
+	#train plda
+	$train_cmd exp/ivector_train_1024/log/plda.log \
+		ivector-compute-plda ark:data/train/spk2utt \
+  	'ark:ivector-normalize-length scp:exp/ivector_train_1024/ivector.scp  ark:- |' \
+		exp/ivector_train_1024/plda
+fi
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+	#split the test to enroll and eval
+	mkdir -p data/test/enroll data/test/eval
+	cp data/test/{spk2utt,feats.scp,vad.scp} data/test/enroll
+	cp data/test/{spk2utt,feats.scp,vad.scp} data/test/eval
+	local/split_data_enroll_eval.py data/test/utt2spk  data/test/enroll/utt2spk  data/test/eval/utt2spk
+
+	trials=data/test/speaker_ver.lst
+
+	local/produce_trials.py data/test/eval/utt2spk $trials
+	utils/fix_data_dir.sh data/test/enroll
+	utils/fix_data_dir.sh data/test/eval
+
+	fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+	#extract enroll ivector
+	sid/extract_ivectors.sh --cmd "$train_cmd" --nj $nj \
+		  exp/extractor_1024 data/test/enroll  exp/ivector_enroll_1024
+	#extract eval ivector
+	sid/extract_ivectors.sh --cmd "$train_cmd" --nj $nj \
+		  exp/extractor_1024 data/test/eval  exp/ivector_eval_1024
+
+fi
+
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+
+	#compute plda score
+	$train_cmd exp/ivector_eval_1024/log/plda_score.log \
+		ivector-plda-scoring --num-utts=ark:exp/ivector_enroll_1024/num_utts.ark \
+  		exp/ivector_train_1024/plda \
+  		ark:exp/ivector_enroll_1024/spk_ivector.ark \
+  		"ark:ivector-normalize-length scp:exp/ivector_eval_1024/ivector.scp ark:- |" \
+  		"cat '$trials' | awk '{print \\\$2, \\\$1}' |" exp/trials_out
+
+	#compute eer
+	awk '{print $3}' exp/trials_out | paste - $trials | awk '{print $1, $4}' | compute-eer -
+	exit 0
+fi
